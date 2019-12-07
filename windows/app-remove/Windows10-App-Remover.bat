83c0515c664ac4d4377d4eda10f1ba574a54a821
seochan2 <seochan2@gmail.com>
2019-12-07 11:51:24 +0000

    update list of apps to be removed

--- windows/app-remove/Windows10-App-Remover.bat
+++ windows/app-remove/Windows10-App-Remover.bat
@@ -3,8 +3,14 @@
 echo.
 
 SET isSkype=true
+SET isXbox_Group=true
 
 IF "%isSkype%"=="true" (
     echo "Remove App - Skype"
     powershell.exe -Command "Get-AppxPackage -AllUsers Microsoft.SkypeApp | Remove-AppxPackage"
+)
+
+IF "%isXbox_Group%"=="true" (
+    echo "Remove App - isXbox_Group"
+    powershell.exe -Command "Get-AppxPackage -AllUsers *Xbox* | Remove-AppxPackage"
 )
